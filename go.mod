2e449b49d9d013fa480247d185ab82ded5c3d66a
Janith Leanage <janithleanage@gmail.com>
2019-07-13 08:06:21 +0530

    copied eliza and localised

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/janithl/batagoda
 
 go 1.12
 
-require (
-	github.com/kennysong/goeliza v0.0.0-20160420092726-bdfccfe37f9c
-	gopkg.in/tucnak/telebot.v2 v2.0.0-20190415090633-8c1c512262f2
-)
+require gopkg.in/tucnak/telebot.v2 v2.0.0-20190415090633-8c1c512262f2
--- main.go
+++ main.go
@@ -7,25 +7,9 @@ import (
 	"strings"
 	"time"
 
-	liz "github.com/kennysong/goeliza"
+	liz "github.com/janithl/batagoda/eliza"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
-
-var greetings = []string{
-	"මීට කලින් මාව දැකල නැද්ද?",
-	"ඇයි පකෝ?",
-	"තොට ඇම්ම කියල මම පලිද?",
-	"මොකද හුත්තො අනින්නෙ?",
-	"තොගෙ අඳෝනාව අහන් ඉඳල මට බඩ යනව",
-	"මේකට වඩා හොඳයි ඇට වල මවිල් ගනං කරන එක",
-	"ගිහිං වැලක් බලපංකො පව් නොදී",
-	"තොපිගෙ මුල් ලමාවිය ප්‍රශ්න තමයි පෙන්නන්නෙ",
-}
-
-func greet() string {
-	n := rand.Intn(len(greetings))
-	return greetings[n]
-}
 
 func main() {
 	var (
@@ -53,12 +37,8 @@ func main() {
 		rand.Seed(time.Now().Unix())
 		if strings.Contains(strings.ToLower(m.Text), "බටගොඩ") ||
 			strings.Contains(strings.ToLower(m.Text), "batagoda") {
-			if rand.Intn(3) == 2 {
-				b.Send(m.Chat, greet())
-			} else {
-				prompt := strings.ReplaceAll(strings.ToLower(m.Text), "batagoda", "")
-				b.Send(m.Chat, liz.ReplyTo(prompt))
-			}
+			prompt := strings.ReplaceAll(strings.ToLower(m.Text), "batagoda", "")
+			b.Send(m.Chat, liz.ReplyTo(prompt))
 		}
 	})
 
