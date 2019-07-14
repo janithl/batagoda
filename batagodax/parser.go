671dd1eb9f1c29796cf9270228a14887a27076de
Janith Leanage <janithleanage@gmail.com>
2019-07-14 21:15:32 +0530

    wrote func to remove ZWJ char weirdness

--- batagodax/parser.go
+++ batagodax/parser.go
@@ -7,10 +7,12 @@ import (
 
 // Respond responds to a prompt
 func Respond(prompt string) string {
+	prompt = cleanString(prompt)
+
 	// Iterates through Rules, tries to match to prompt
 	for _, rule := range Rules {
 		for _, trigger := range rule.Triggers {
-			if strings.Contains(prompt, trigger) {
+			if strings.Contains(prompt, cleanString(trigger)) {
 				return randChoice(rule.Responses)
 			}
 		}
@@ -18,6 +20,25 @@ func Respond(prompt string) string {
 
 	// If all else fails, return a fallback
 	return randChoice(Fallbacks)
+}
+
+// cleanString cleans out special unicode chars from strings
+func cleanString(str string) string {
+	const (
+		ZWSP   = '\u200B'
+		ZWNBSP = '\uFEFF'
+		ZWJ    = '\u200D'
+		ZWNJ   = '\u200C'
+	)
+
+	r := strings.NewReplacer(
+		string(ZWSP), "",
+		string(ZWNBSP), "",
+		string(ZWJ), "",
+		string(ZWNJ), "",
+	)
+
+	return r.Replace(str)
 }
 
 // randChoice returns a random element in an (string) array.
